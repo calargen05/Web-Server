f2171f8b9bf4916a4b7efcf5db210c50abe8e993
calargen05 <calargen18@gmail.com>
2025-06-26 23:38:58 -0400

    inital commit

---
+++ server.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/*
+ * STEPS FOR THIS PROJECT:
+ *      1. Create the Listening Socket
+ *      2. Handle Single HTTP Request
+ *      3. Serve Files From my File System
+ *      4. Handle Multiple Clients at the Same Time
+ *
+ *
+ *      1. Create the Listening Socket
+ *          step 1: make a socket
+ *          step 2: bind the socke to an address
+ *          step 3: listen for connections
+ *          step 4: accept the connection
+ *          step 5: close the file socket
+ *
+ *      I will continue the steps as I continue through the project
+ */
+
+int main() {
+    
+    return 0;
+}
